1eb616e63097a289fdf9e2aee7f9ee090e2912a5
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-11-15 22:00:53 -0800

    Add part01-08_name_and_address

---
+++ part01/part01-08_name_and_address/name_and_address.py
@@ -0,0 +1,8 @@
+first_name = input("Given name: ")
+last_name = input("Family name: ")
+address = input("Street address: ")
+city_and_postal = input("City and postal code: ")
+
+print(first_name + " " + last_name)
+print(address)
+print(city_and_postal)
