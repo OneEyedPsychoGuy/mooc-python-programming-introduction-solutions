f23ba96a7bc084bda4c7ffbd152213649098d8df
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-11-18 23:17:25 -0800

    Add part02-12_leap_year

---
+++ part02/part02-12_leap_year/leap_year.py
@@ -0,0 +1,8 @@
+year = int(input("Please type in a year: "))
+
+if year % 400 == 0:
+    print("This year is a leap year.")
+elif year % 4 == 0 and year % 100 != 0:
+    print("This year is a leap year.")
+else:
+    print("Thiss year is not a leap year.")
