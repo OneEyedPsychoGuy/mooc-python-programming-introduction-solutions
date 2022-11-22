9645eef3cf64c3c89f4175c1e0e7787ee927e1b7
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-11-21 22:17:45 -0800

    Add part02-22_working_with_numbers Part 1: Count

--- part02/part02-22_working_with_numbers/working_with_numbers.py
+++ part02/part02-22_working_with_numbers/working_with_numbers.py
@@ -1,6 +1,10 @@
-print("Please type in integer numbers. Type in 0 to finish.")
+count = 0
 
+print("Please type in integer numbers. Type in 0 to finish.")
 while True:
     num = int(input("Number: "))
     if num == 0:
         break
+    count += 1
+
+print(f"Numbers typed in {count}")
