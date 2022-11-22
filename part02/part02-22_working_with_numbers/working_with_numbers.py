5fea135138eab2452a0d0a48c2ca4c98b9dadd08
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-11-21 22:24:23 -0800

    Add part02-22_working_with_numbers Part 4: Positives and negatives

--- part02/part02-22_working_with_numbers/working_with_numbers.py
+++ part02/part02-22_working_with_numbers/working_with_numbers.py
@@ -1,5 +1,4 @@
-count = 0
-sum = 0
+count, sum, pos = 0, 0, 0
 
 print("Please type in integer numbers. Type in 0 to finish.")
 while True:
@@ -8,7 +7,11 @@ while True:
         break
     count += 1
     sum += num
+    if num > 0:
+        pos += 1
 
 print(f"Numbers typed in {count}")
 print(f"The sum of the numbers is {sum}")
 print(f"The mean of the numbers is {sum / count}")
+print(f"Positive numbers {pos}")
+print(f"Negative numbers {count - pos}")
