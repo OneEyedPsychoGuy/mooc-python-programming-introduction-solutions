d758369765707b9b6ab4a6576f897e08986ecec2
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-11-26 21:04:55 -0800

    Add part03-33_chessboard

---
+++ part03/part03-33_chessboard/chessboard.py
@@ -0,0 +1,9 @@
+def chessboard(length):
+    rows = 0
+    while rows < length:
+        if rows % 2 == 0:
+            row = "10" * length
+        else:
+            row = "01" * length
+        print(row[0:length])
+        rows += 1
