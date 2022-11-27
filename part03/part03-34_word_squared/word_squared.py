303aa815479796e3e3251972726dc7ad4d294f2f
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-11-26 23:16:26 -0800

    Add part03-34_word_squared

---
+++ part03/part03-34_word_squared/word_squared.py
@@ -0,0 +1,12 @@
+def squared(text, size):
+    rows = 0
+    index = 0
+    while rows < size:
+        row = ""
+        while len(row) < size:
+            if index >= len(text):
+                index = 0
+            row += text[index]
+            index += 1
+        print(row)
+        rows += 1
