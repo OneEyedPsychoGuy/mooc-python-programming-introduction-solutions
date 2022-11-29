3fa23077bd33a4572ced9de23439fc22279d5da4
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-11-28 22:51:01 -0800

    Add part04-08_spruce

---
+++ part04/part04-08_sspruce/spruce.py
@@ -0,0 +1,9 @@
+def spruce(size):
+    print("a spruce!")
+    rows = 1
+    while rows <= size:
+        spaces = " " * (size - rows)
+        stars = "*" * (rows * 2 - 1)
+        print(f"{spaces}{stars}{spaces}")
+        rows += 1
+    print(f"{' ' * (size - 1)}*{' ' * (size - 1)}")
