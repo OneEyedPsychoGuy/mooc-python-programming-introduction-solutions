a4b7457347edba15941a4580a7e68a6ad8d94009
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-11-30 21:09:36 -0800

    Add part04-24_palindromes

---
+++ part04/part04-24_palindromes/palindromes.py
@@ -0,0 +1,9 @@
+def palindromes(word):
+    return word == word[::-1]
+
+while True:
+    word = input("Please type in a palindrome: ")
+    if palindromes(word):
+        print(f"{word} is a palindrome!")
+        break
+    print("that wasn't a palindrome")
