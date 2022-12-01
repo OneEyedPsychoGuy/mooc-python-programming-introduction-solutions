7a78f18a2c6eba9f211b85ec0dc0034292e1e07a
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-11-30 21:52:53 -0800

    Add part04-31_all_longest_in_list

---
+++ part04/part04-31_all_longest_in_list/all_longest_in_list.py
@@ -0,0 +1,12 @@
+def all_the_longest(words):
+    longest_words = []
+    longest = -1
+
+    for word in words:
+        if len(word) > longest:
+            longest = len(word)
+    for word in words:
+        if len(word) == longest:
+            longest_words.append(word)
+    
+    return longest_words
