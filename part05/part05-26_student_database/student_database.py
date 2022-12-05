798212692c4a7796632768de1d8d967478fb37ee
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-04 18:15:41 -0800

    Add part05-26_student_database Part 3: repeated courses

--- part05/part05-26_student_database/student_database.py
+++ part05/part05-26_student_database/student_database.py
@@ -1,8 +1,18 @@
 def add_student(students: dict, name: str) -> None:
     students[name] = []
 
-def add_course(students: dict, name: str, course: tuple[str, int]) -> None:
-    students[name].append(course)
+def add_course(students: dict, name: str, course_to_add: tuple[str, int]) -> None:
+    if course_to_add[1] == 0:
+        return
+    
+    for course in students[name]:
+        if course[0] == course_to_add[0]:
+            if course[1] > course_to_add[1]:
+                return
+            else:
+                students[name].remove(course)
+                break
+    students[name].append(course_to_add)
 
 def print_student(students: dict, name: str) -> None:
     if name in students:
@@ -25,4 +35,6 @@ students = {}
 add_student(students, "Peter")
 add_course(students, "Peter", ("Introduction to Programming", 3))
 add_course(students, "Peter", ("Advanced Course in Programming", 2))
+add_course(students, "Peter", ("Data Structures and Algorithms", 0))
+add_course(students, "Peter", ("Introduction to Programming", 2))
 print_student(students, "Peter")
