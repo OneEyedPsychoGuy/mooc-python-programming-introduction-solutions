67d8db5cdcb8e452581b2b7547e0d301d933f8ff
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-04 18:05:00 -0800

    Add part05-26_student_database Part 2: adding completed courses

--- part05/part05-26_student_database/student_database.py
+++ part05/part05-26_student_database/student_database.py
@@ -1,15 +1,28 @@
 def add_student(students: dict, name: str) -> None:
     students[name] = []
 
+def add_course(students: dict, name: str, course: tuple[str, int]) -> None:
+    students[name].append(course)
+
 def print_student(students: dict, name: str) -> None:
     if name in students:
-        print(f"{name}:\n no completed courses")
+        text = f"{name}:\n "
+        courses = students[name]
+        if len(courses) == 0:
+            text += "no completed courses"
+        else:
+            text += f"{len(courses)} completed courses:\n"
+            sum = 0
+            for course in courses:
+                text += f"  {course[0]} {course[1]}\n"
+                sum += course[1]
+            text += f" average grade {sum / len(courses)}"
+        print(text)
     else:
         print(f"{name}: no such person in the database")
 
 students = {}
 add_student(students, "Peter")
-add_student(students, "Eliza")
+add_course(students, "Peter", ("Introduction to Programming", 3))
+add_course(students, "Peter", ("Advanced Course in Programming", 2))
 print_student(students, "Peter")
-print_student(students, "Eliza")
-print_student(students, "Jack")
