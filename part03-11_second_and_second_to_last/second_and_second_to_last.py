b8b1061723e10d90ea4faf9dd4dfcc6ccd13e8e3
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-10 23:53:24 -0800

    Finalize part03-11_second_and_second_to_last

--- part03-11_second_and_second_to_last/second_and_second_to_last.py
+++ part03-11_second_and_second_to_last/second_and_second_to_last.py
@@ -1,6 +1,6 @@
 text = input("Please type in a string: ")
 
-if text[1] == text[len(text) - 2]:
+if len(text) > 1 and text[1] == text[-2]:
     print(f"The second and the second to last characters are {text[1]}")
 else:
     print("The second and the second to last characters are different")
