754871a32ac0935e7e5baa9b31f98b8d5945c729
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-11 00:18:32 -0800

    Finalize part04-12_change_value_of_item

--- part04-12_change_value_of_item/change_value_of_item.py
+++ part04-12_change_value_of_item/change_value_of_item.py
@@ -3,6 +3,8 @@ while True:
     index = int(input("Index: "))
     if index == -1:
         break
+    if index < 0 or index >= len(nums):
+        print("Index is out of range")
     value = int(input("New value: "))
     nums[index] = value
     print(nums)
