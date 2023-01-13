97d241bf511fd2d38bcb16d4848080a065117a42
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-12 23:18:06 -0800

    Finalize part04-04_square_of_hashes

--- part04-04_square_of_hashes/square_of_hashes.py
+++ part04-04_square_of_hashes/square_of_hashes.py
@@ -1,9 +1,9 @@
-def line(times, characters):
-    if characters == "":
-        characters = "*"
-    print(characters[0] * times)
+def line(times: int, character: str):
+    if character == "":
+        character = "*"
+    print(character[0] * times)
 
-def square_of_hashes(length):
+def square_of_hashes(length: int):
     height = length
     while height > 0:
         line(length, "#")
