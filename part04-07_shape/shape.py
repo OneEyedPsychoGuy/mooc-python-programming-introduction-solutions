d68aa7f9d4b738a93f503d720e1c5c41b1999bb3
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-12 23:21:51 -0800

    Finalize part04-07_shape

--- part04-07_shape/shape.py
+++ part04-07_shape/shape.py
@@ -1,19 +1,19 @@
-def line(times, characters):
-    if characters == "":
-        characters = "*"
-    print(characters[0] * times)
+def line(times: int, character: str):
+    if character == "":
+        character = "*"
+    print(character[0] * times)
 
-def rectangle(length, height, character):
+def rectangle(length: int, height: int, character: str):
     while height > 0:
         line(length, character)
         height -= 1
 
-def triangle(height, character):
+def triangle(height: int, character: str):
     length = 1
     while length <= height:
         line(length, character)
         length += 1
 
-def shape(length, tri_char, height, rect_char):
+def shape(length: int, tri_char: str, height: int, rect_char: str):
     triangle(length, tri_char)
     rectangle(length, height, rect_char)
