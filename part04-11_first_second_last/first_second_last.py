fb5f22ffd1cde7c9653ecacd73e8abd1e79796ef
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-12 23:25:37 -0800

    Finalize part04-11_first_second_last

--- part04-11_first_second_last/first_second_last.py
+++ part04-11_first_second_last/first_second_last.py
@@ -1,9 +1,9 @@
-def first_word(sentence):
+def first_word(sentence: str):
     return sentence.split(" ")[0]
 
-def second_word(sentence):
+def second_word(sentence: str):
     return sentence.split(" ")[1]
 
-def last_word(sentence):
+def last_word(sentence: str):
     words = sentence.split(" ")
     return words[len(words) - 1]
