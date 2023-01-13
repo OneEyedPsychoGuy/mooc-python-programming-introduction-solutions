1977d17fb42ed03de0491b3eba412d7a4281a46e
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-12 23:19:05 -0800

    Finalize part04-05_square

--- part04-05_square/square.py
+++ part04-05_square/square.py
@@ -1,9 +1,9 @@
-def line(times, characters):
-    if characters == "":
-        characters = "*"
-    print(characters[0] * times)
+def line(times: int, character: str):
+    if character == "":
+        character = "*"
+    print(character[0] * times)
 
-def square(length, character):
+def square(length: int, character: str):
     height = length
     while height > 0:
         line(length, character)
