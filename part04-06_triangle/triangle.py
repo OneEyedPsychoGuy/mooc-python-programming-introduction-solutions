c77d417fee9019def623eeee2521d58ff57e6f61
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-12 23:20:03 -0800

    Finalize part04-06_triangle

--- part04-06_triangle/triangle.py
+++ part04-06_triangle/triangle.py
@@ -1,9 +1,9 @@
-def line(times, characters):
-    if characters == "":
-        characters = "*"
-    print(characters[0] * times)
+def line(times: int, character: str):
+    if character == "":
+        character = "*"
+    print(character[0] * times)
 
-def triangle(height):
+def triangle(height: int):
     length = 1
     while length <= height:
         line(length, "#")
