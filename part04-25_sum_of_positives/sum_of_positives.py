2287361a606df151e119412432a531d18d2270a3
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-12 23:33:18 -0800

    Finalize part04-25_sum_of_positives

--- part04-25_sum_of_positives/sum_of_positives.py
+++ part04-25_sum_of_positives/sum_of_positives.py
@@ -1,6 +1,6 @@
-def sum_of_positives(numbers):
+def sum_of_positives(nums: list[int]):
     sum = 0
-    for num in numbers:
+    for num in nums:
         if num > 0:
             sum += num
     return sum
