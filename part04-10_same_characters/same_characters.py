e8cd365f17c390db2b0f71e197233629dd346453
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-14 06:40:06 -0800

    Finalize part04-10_same_character

--- part04-10_same_characters/same_characters.py
+++ part04-10_same_characters/same_characters.py
@@ -1,4 +1,4 @@
-def same_chars(word: str, index1: int, index2: int):
+def same_chars(word: str, index1: int, index2: int) -> bool:
     if index1 >= len(word) or index2 >= len(word):
         return False
     return word[index1] == word[index2]
