64c68321ea44eaf66bfce3bd5c2672cacdd8d257
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-14 06:56:16 -0800

    Finalize part04-33_everything_reversed

--- part04-33_everything_reversed/everything_reversed.py
+++ part04-33_everything_reversed/everything_reversed.py
@@ -1,4 +1,4 @@
-def everything_reversed(strings: list[str]):
+def everything_reversed(strings: list[str]) -> list[str]:
     reversed = []
     for string in strings[::-1]:
         reversed.append(string[::-1])
