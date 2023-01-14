06722441ccd978d1bbbcdf585ab826c969a70db0
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-14 06:49:40 -0800

    Finalize part04-27_sum_of_lists

--- part04-27_sum_of_lists/sum_of_lists.py
+++ part04-27_sum_of_lists/sum_of_lists.py
@@ -1,4 +1,4 @@
-def list_sum(nums1: list[int], nums2: list[int]):
+def list_sum(nums1: list[int], nums2: list[int]) -> list[int]:
     sums = []
     for i in range(len(nums1)):
         sums.append(nums1[i] + nums2[i])
