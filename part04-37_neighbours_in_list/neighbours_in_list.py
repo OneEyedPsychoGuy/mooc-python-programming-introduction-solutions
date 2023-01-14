d59ac48628fdae5f5628dccfe34945ac7acfbdce
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-14 06:24:52 -0800

    Finalize part04-37_neighbours_in_list

--- part04-37_neighbours_in_list/neighbours_in_list.py
+++ part04-37_neighbours_in_list/neighbours_in_list.py
@@ -1,9 +1,9 @@
-def longest_series_of_neighbours(numbers):
+def longest_series_of_neighbours(nums: list[int]):
     longest = 1
     length = 1
 
-    for i in range(len(numbers) - 1):
-        if abs(numbers[i] - numbers[i + 1]) == 1:
+    for i in range(len(nums) - 1):
+        if abs(nums[i] - nums[i + 1]) == 1:
             length += 1
         else:
             length = 1
