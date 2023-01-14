ba2cf235af07927f874d5961279d4af938c799e4
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-14 06:57:08 -0800

    Finalize part04-34_most_common_character

--- part04-34_most_common_character/most_common_character.py
+++ part04-34_most_common_character/most_common_character.py
@@ -1,4 +1,4 @@
-def most_common_character(string: str):
+def most_common_character(string: str) -> str:
     common = string[0]
     for char in string:
         if string.count(char) > string.count(common):
