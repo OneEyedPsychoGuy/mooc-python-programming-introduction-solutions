5b6369bf7bd76093e89feae8adb9fd2e17f5d2fe
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-14 06:33:52 -0800

    Finalize part04-03_box_of_hashes

--- part04-03_box_of_hashes/box_of_hashes.py
+++ part04-03_box_of_hashes/box_of_hashes.py
@@ -1,9 +1,9 @@
-def line(times: int, character: str):
+def line(times: int, character: str) -> None:
     if character == "":
         character = "*"
     print(character[0] * times)
 
-def box_of_hashes(height: int):
+def box_of_hashes(height: int) -> None:
     while height > 0:
         line(10, "#")
         height -= 1
