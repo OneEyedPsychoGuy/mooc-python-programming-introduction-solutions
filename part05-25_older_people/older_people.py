af8128badab55947c2a39a93e935dec2e19bb8b6
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-15 18:14:46 -0800

    Finalize part05-25_older_people

--- part05-25_older_people/older_people.py
+++ part05-25_older_people/older_people.py
@@ -1,4 +1,4 @@
-def older_people(people: list[tuple[str, int]], year: int) -> list[str]:
+def older_people(people: list[tuple[str|int]], year: int) -> list[str]:
     old = []
     for person in people:
         if person[1] < year:
