d73200af6c7aba3ef2ff210f17cdb4a53e8129a1
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-15 18:07:41 -0800

    Finalize part05-21_movie_database

--- part05-21_movie_database/movie_database.py
+++ part05-21_movie_database/movie_database.py
@@ -1,4 +1,4 @@
-def add_movie(db: list[dict], name: str, director: str, year: int, runtime: int) -> None:
+def add_movie(db: list[dict[str, str|int]], name: str, director: str, year: int, runtime: int) -> None:
     db.append({
         "name": name,
         "director": director,
