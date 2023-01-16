3e039eb672566eaf25e4f40e2d98a4db670e1d78
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-15 18:09:16 -0800

    Finalize part05-23_create_tuple

--- part05-23_create_tuple/create_tuple.py
+++ part05-23_create_tuple/create_tuple.py
@@ -1,4 +1,4 @@
-def create_tuple(x: int, y: int, z: int) -> tuple[int, int, int]:
+def create_tuple(x: int, y: int, z: int) -> tuple[int]:
     return (
         min(x, y, z),
         max(x, y, z),
