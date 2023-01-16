f417832146a28eb652e8e74ff59987daf66f70c2
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-15 18:08:29 -0800

    Finalize part05-22_find_movies

--- part05-22_find_movies/find_movies.py
+++ part05-22_find_movies/find_movies.py
@@ -1,4 +1,4 @@
-def find_movies(db: list[dict], search: str) -> list[dict]:
+def find_movies(db: list[dict[str, str|int]], search: str) -> list[dict[str, str|int]]:
     results = []
     for movie in db:
         if search.lower() in movie["name"].lower():
