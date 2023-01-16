34fda981ffa0f09c9739d4351332eaa4f3aa6203
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-15 18:11:13 -0800

    Finalize part05-24_oldest_person

--- part05-24_oldest_person/oldest_person.py
+++ part05-24_oldest_person/oldest_person.py
@@ -1,4 +1,4 @@
-def oldest_person(people: list[tuple[str, int]]) -> str:
+def oldest_person(people: list[tuple[str|int]]) -> str:
     oldest = people[0]
     for person in people:
         if person[1] < oldest[1]:
